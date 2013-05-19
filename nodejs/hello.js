4dbcfcd526e212f9aee70d2141620d700e8a2017
isuzuki <isuzuki@suzukiikuo-no-MacBook-Air.local>
2013-05-19 20:37:14 +0900

    nodejs追加

---
+++ nodejs/hello.js
@@ -0,0 +1,12 @@
+var http = require('http');
+
+
+http.createServer(function(req, res) {
+	console.log('Hello World');
+
+	res.writeHead(200, {'Content-Type': 'text/plain'});
+	res.write('Hello World');
+	res.end();
+}).listen(8888);
+
+console.log('Server Runngin at http://127.0.0.1:8888/');
