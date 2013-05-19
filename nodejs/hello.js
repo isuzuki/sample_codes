cb43c95d499735d2f5bf85a271e10a254f6deb4b
isuzuki <isuzuki@suzukiikuo-no-MacBook-Air.local>
2013-05-19 20:36:39 +0900

    コミットファイル間違いなので削除

--- nodejs/hello.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var port = 8124; 
-
-http.createServer(function (req, res) {
-	res.writeHead(200, {'Content-Type': 'text/plain'});
-	res.end('Hello World!\n');
-}).listen(port);
- 
-console.log('Server running at http://127.0.0.1:' + port);
