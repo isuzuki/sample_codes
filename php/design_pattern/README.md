5d3accb10234035abfdc6d54fcf4fe04ac8198f4
isuzuki <isuzuki@suzukiikuonoair.lan>
2014-05-14 02:16:36 +0900

    参考にするサイトを変えたので一度削除

--- php/design_pattern/README.md
+++
@@ -1,3 +0,0 @@
-## デザインパターンの勉強
-参考URL : http://www.techscore.com/tech/DesignPattern/  
-上記サイトのコードをPHPで書いてみたもの
--- php/design_pattern/iterator/Teacher.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-abstract class Teacher {
-
-	abstract public function create_student_list();
-
-	abstract public function call_students();
-}
