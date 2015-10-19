36c94ba7d1e726809a715083a757d0803a39cc62
isuzuki <isuzuki@suzukiikuonoAir.lan>
2015-10-20 02:06:40 +0900

    [PHP] add phpunit sample

---
+++ php/phpunit/Test.php
@@ -0,0 +1,43 @@
+<?php
+
+class Test extends PHPUnit_Framework_TestCase
+{
+    public function testPushAndPop()
+    {
+        $stack = [];
+        $this->assertEquals(0, count($stack));
+
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[0]);
+        $this->assertEquals(1, count($stack));
+
+        $this->assertEquals('foo', array_pop($stack));
+        $this->assertEquals(0, count($stack));
+    }
+
+    public function testEmpty()
+    {
+        $stack = [];
+        $this->assertEmpty($stack);
+
+        return $stack;
+    }
+
+    /**
+     * @depends testEmpty
+     */
+    public function testPush(array $stack)
+    {
+        array_push($stack, 'foo');
+        $this->assertEquals('foo', $stack[0]);
+        $this->assertNotEmpty($stack);
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testException()
+    {
+        throw new InvalidArgumentException();
+    }
+}
