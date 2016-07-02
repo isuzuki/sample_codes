5df76e1d16ef05d4fc887efee084aaaf9bc4ccc0
isuzuki <isuzuki@suzukiikuo-no-MacBook-Air.local>
2016-07-02 15:05:41 +0900

    [python]add basic samples

---
+++ python/basic/if.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python
+# coding: UTF-8¬
+
+# 大小チェック
+num = 10
+if 0 <= num and num <= 100:
+    print "true"
+
+# 範囲を論理演算子無しで記述
+if 0 <= num <= 100:
+    print "true"
---
+++ python/basic/with.py
@@ -0,0 +1,20 @@
+#! /usr/bin/python
+# coding: UTF-8¬
+
+# 参考 http://qiita.com/howmuch/items/95efab2e61f735a150e3
+
+# with使用
+with open('text1.txt', 'w') as text:
+    text.write('Hello World')
+
+# with不使用
+text = None
+try:
+    text = open('text2.txt', 'w')
+    try:
+        text.write('Hello World')
+    except:
+        raise
+finally:
+    if text:
+        text.close()
